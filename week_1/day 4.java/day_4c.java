4e505e2cc00ca6beeeec37107c3566e27aaf1bde
Sukriti-glb <sukriti7.kriti@gmail.com>
2026-06-04 10:43:18 +0530

    added spiral matrix problem

---
+++ week_1/day 4.java/day_4c.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+       int rowEnd = matrix.length - 1;
+       int colEnd = matrix[0].length - 1;
+       List<Integer> ans = new ArrayList<>();
+       int rowBegin = 0;
+int colBegin = 0;
+        while(rowBegin<=rowEnd&&colBegin<=colEnd){
+            for(int j=colBegin;j<=colEnd;j++){
+                ans.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+            for(int j=rowBegin;j<=rowEnd;j++){
+                ans.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+            if(rowBegin<=rowEnd){
+                 for(int j=colEnd;j>=colBegin;j--){
+               ans.add(matrix[rowEnd][j]);
+            }
+            rowEnd--;
+            if(colBegin<=colEnd){
+             for(int j=rowEnd;j>=rowBegin;j--){
+               ans.add(matrix[j][colBegin]);
+                }
+                colBegin++;
+                 }
+            }
+        }
+        return ans;
+    }
+}
+
