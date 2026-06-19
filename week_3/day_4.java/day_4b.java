84b060fd6122e75d2e6918a828630801fe5f566d
Sukriti-glb <sukriti7.kriti@gmail.com>
2026-06-19 11:31:34 +0530

    added reverse polish problem

--- week_3/day_4.java/day_4b.java
+++ week_3/day_4.java/day_4b.java
@@ -1,3 +1,33 @@
-public class day_4b {
-    
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack= new Stack<>();
+        for(String token:tokens){
+            if(isOp(token)){
+                int num2=stack.pop();
+                int num1=stack.pop();
+                int result=0;
+                  if(token.equals("+")){
+                    result=num1+num2;
+                    }
+                else if(token.equals("-")){
+                    result=num1-num2;
+                } else if(token.equals("*")){
+                    result=num1*num2;
+                }
+                 else if(token.equals("/")){
+                    result=num1/num2;
+                }
+                stack.push(result);
+            }
+                else
+                    stack.push(Integer.parseInt(token));
+            }
+        return stack.peek();
+    }
+    private boolean isOp(String str){
+        if(str.equals("+")||str.equals("-")||str.equals("*")||str.equals("/")){
+        return true;
+    }
+    return false;
+}
 }
